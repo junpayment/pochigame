b382a10d46ad5ef990158491e8dbde663d881e61
Junpei Nomura <junpei.nomura@drecom.co.jp>
2017-09-06 21:34:50 +0900

    ルート設定を追加

--- Gemfile
+++ Gemfile
@@ -50,3 +50,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'squasher'
 
 gem 'rails-erd'
+
+gem 'json-schema'
+gem 'prmd'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # スタミナ回復
+  resource :recover, only: [:index] do
+    collection do
+      post :exec
+    end
+  end
+
+  # クエスト
+  resources :quests, only: [:index] do
+    member do
+      post :exec
+    end
+  end
+
+  # ガチャ
+  resources :gachas, only: [:index] do
+    member do
+      post :exec
+    end
+  end
+
+  # ガチャ
+  resources :gachas, only: [:index] do
+    member do
+      post :exec
+    end
+  end
+
+  # デッキ
+  resource :deck, only: [:index, :create, :update, :destroy]
+
+  # キャラ
+  resource :character, only: [:index, :show] do
+    member do
+      post :sell
+      post :combine
+    end
+  end
+
+  # ユーザ情報
+  resource :user, only: [:index]
 end
